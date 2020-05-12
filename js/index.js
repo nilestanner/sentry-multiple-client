29c75561ec8d845e4970bb9cb92cf35765152773
Niles Tanner <niles.tanner@gmail.com>
2020-05-11 23:26:42 -0600

    first commit

---
+++ js/index.js
@@ -0,0 +1,33 @@
+require('dotenv').config();
+const Sentry = require('@sentry/node');
+
+
+// create clients
+const client1 = new Sentry.NodeClient({
+  dsn: process.env.PROJECT_1_DSN,
+});
+
+const client2 = new Sentry.NodeClient({
+  dsn: process.env.PROJECT_2_DSN,
+});
+
+// create hubs with clients
+const project1 = new Sentry.Hub(client1);
+const project2 = new Sentry.Hub(client2);
+
+// add any tags to each project
+project1.run((currentHub) => {
+  project1.configureScope((scope) => {
+    scope.setTag('project', 1);
+  });
+});
+
+project1.captureMessage('hello world 1');
+
+project2.run((currentHub) => {
+  project2.configureScope((scope) => {
+    scope.setTag('project', 2);
+  });
+});
+
+project2.captureMessage('hello world 2');
